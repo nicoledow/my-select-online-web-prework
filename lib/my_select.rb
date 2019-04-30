73ff9770d184ab6391c556a9c5509b16e455ee38
Nicole Dow <nikki.dow71@gmail.com>
2019-04-30 21:29:38 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  true_elements = []
+  
+  while i < collection.length
+    if yield(collection[i]) == true
+      true_elements << collection[i]
+    end
+    i += 1
+  end
+  return true_elements
 end
